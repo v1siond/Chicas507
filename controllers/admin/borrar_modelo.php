426d14acbd3c06c09e7468caa03b281f4d58cd1a
v1siond <alexanderpulido81@hotmail.com>
2017-07-24 21:33:13 -0400

    fix delete model, and other updates

--- controllers/admin/borrar_modelo.php
+++ controllers/admin/borrar_modelo.php
@@ -18,21 +18,47 @@
 		header('Location: http://www.chicas507.com/error.php');
 	}else{
 
-		$consulta1 = $conexion->prepare('SELECT status FROM modelo WHERE codigo = :user LIMIT 1');
+		$consulta1 = $conexion->prepare('SELECT codigo FROM modelo WHERE codigo = :user LIMIT 1');
 		$consulta1->execute(array(':user' => $id));
 		$result = $consulta1->fetchAll();
 
 		if(!$result){
 			header('Location: http://www.chicas507.com/error.php');
 		}else{
-			$consulta = $conexion->prepare('DELETE FROM modelo WHERE codigo = :id');
-			$consulta->execute(array(':id' => $id));
-			$error = $consulta->errorInfo();
+			$delete_photo = $conexion->prepare('DELETE FROM fotos_modelo WHERE modelo = :id');
+			$delete_photo->execute(array(':id' => $id));
+			$error = $delete_photo->errorInfo();
 			foreach ($error as $errores) {
 				if(empty($errores) || $error == 0){
 					$error = '';
 					header('Location: http://www.chicas507.com/exito.php');
 				}else{
+					echo $errores;
+					header('Location: http://www.chicas507.com/error.php');
+				}
+			}
+
+			$delete_video = $conexion->prepare('DELETE FROM video_modelo WHERE modelo_video = :id');
+			$delete_video->execute(array(':id' => $id));
+			$error = $delete_video->errorInfo();
+			foreach ($error as $errores) {
+				if(empty($errores) || $error == 0){
+					$error = '';
+					header('Location: http://www.chicas507.com/exito.php');
+				}else{
+					echo $errores;
+					header('Location: http://www.chicas507.com/error.php');
+				}
+			}
+			$delete_model = $conexion->prepare('DELETE FROM modelo WHERE codigo = :id');
+			$delete_model->execute(array(':id' => $id));
+			$error = $delete_model->errorInfo();
+			foreach ($error as $errores) {
+				if(empty($errores) || $error == 0){
+					$error = '';
+					header('Location: http://www.chicas507.com/exito.php');
+				}else{
+					echo $errores;
 					header('Location: http://www.chicas507.com/error.php');
 				}
 			}
